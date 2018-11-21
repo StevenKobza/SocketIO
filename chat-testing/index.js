3df3d564ba1a43bbaf3eb7d4f5d5fa4c48d4e351
Steven <32111227+KobzaS@users.noreply.github.com>
2018-11-20 19:55:44 -0800

    Messaging Capabilities

--- chat-testing/index.js
+++ chat-testing/index.js
@@ -8,6 +8,12 @@ app.get("/", function(req, res){
 
 io.on("connection", function(socket){
     console.log("A user connected");
+    socket.on("disconnect", function(){
+        console.log("A user disconnected");
+    });
+    socket.on("chat message", function(msg){
+        console.log("message: " + msg);
+    });
 });
 
 http.listen(3000, function(){
