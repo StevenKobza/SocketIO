b1b2a42a4d564ec63148473c319124bf8cc1e777
Steven <32111227+KobzaS@users.noreply.github.com>
2018-11-20 20:00:08 -0800

    Finished Tutorial

--- chat-testing/index.js
+++ chat-testing/index.js
@@ -7,13 +7,14 @@ app.get("/", function(req, res){
 });
 
 io.on("connection", function(socket){
+    socket.on("chat message", function(msg){
+        io.emit("chat message", msg);
+    });
     console.log("A user connected");
     socket.on("disconnect", function(){
         console.log("A user disconnected");
     });
-    socket.on("chat message", function(msg){
-        console.log("message: " + msg);
-    });
+    
 });
 
 http.listen(3000, function(){
